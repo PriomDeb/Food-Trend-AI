06d0d6df93b4b0824f8806b6591197cd07727534
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2024-07-06 01:08:28 +0600

    Top number of restaurants

--- food_trend_ai.py
+++ food_trend_ai.py
@@ -19,7 +19,11 @@ def load_dataset(path):
 
 df = joblib.load("Food Review Dataset of Bangladesh.joblib")
 
-# print(df.head())
+
+def calculate_positive_percentage(ratings):
+    positive_ratings = ratings[ratings >= 3]
+    score = (len(positive_ratings) / len(ratings)) * 100 if len(ratings) > 0 else 0
+    return score
 
 
 # Sidebar menu with radio buttons
@@ -39,18 +43,38 @@ if selection == "Top Restaurant Based on City":
     unique_cities = df['city'].unique()
     selected_city = st.selectbox("Select City", sorted(unique_cities))
     
+    # Subheader of Selected City
     st.subheader(f"Selected City: {selected_city}")
     
-    n_top_restaurants = st.selectbox("Top Restaurants by Rating", range(1, 11))
+    # Select Number of Top Restaurants to See
+    n_top_restaurants = st.selectbox("Top Restaurants by Rating", range(1, 51))
     
-    filtered_df = df[df['city'] == selected_city].nlargest(n_top_restaurants, 'ratings_int')
+    # Filter Top Number of Restaurants
+    # Filter and sort restaurants by selected city and highest positive ratings percentage
+    filtered_df = df[df['city'] == selected_city].\
+                  groupby('restaurant')['ratings_int'].apply(calculate_positive_percentage).reset_index()
     
     
-    st.subheader(f"Top {n_top_restaurants} Restaurants in {selected_city} by Ratings")
+    filtered_df.columns = ['Restaurants', 'Positive Score']
+    filtered_df = filtered_df.nlargest(n_top_restaurants, 'Positive Score')
+    
+    print(filtered_df.head())
+    # filtered_df['Positive Score'] = filtered_df['Positive Score'].apply(lambda x: f"{x:.2f}%")
+    
+
+    # Display top restaurants based on highest positive ratings percentage with score
+    st.subheader(f"Top {n_top_restaurants} Restaurants in {selected_city} by Positive Ratings Percentage")
     if len(filtered_df) == 0:
-        st.write("No restaurants found for selected city and rating range.")
+        st.write("No restaurants found for selected city.")
     else:
-        st.write(filtered_df[['restaurant', 'ratings_int']])
+        filtered_df = filtered_df.reset_index(drop=True)
+        filtered_df.index += 1
+        
+        # st.table(filtered_df)
+        
+        st.dataframe(filtered_df.style.background_gradient(cmap='Greens', subset=['Positive Score']).\
+                 set_properties(**{'text-align': 'center'}).\
+                 format({'Positive Score': '{:.2f}%'}), width=600)
     
 elif selection == "Dataset Overview":
     st.subheader("Dataset Overview")
