1445c51170926a63e8f8d65f6f9259a81b58c306
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2024-07-06 12:19:27 +0600

    Sentiment analysis based on restaurants

--- food_trend_ai.py
+++ food_trend_ai.py
@@ -7,6 +7,7 @@ import streamlit as st
 import joblib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from wordcloud import WordCloud
 
 DATASET = "Food Review Dataset of Bangladesh.xlsx"
 developer_mode = True
@@ -195,16 +196,13 @@ elif selection == "Search Restaurants":
   
   sentiment_df = pd.DataFrame(sentiment_data)
   
-  st.write("#### Sentiment in Bar Chart")
-  # st.bar_chart(sentiment_df.set_index('Sentiment'))
-  
   fig = plt.figure()
   ax = fig.add_subplot(111, projection='3d')
   
   x = [0, 1, 2]
   y = [0, 0, 0]
   z = [0, 0, 0]
-  dx = dy = [0.5, 0.5, 0.5]
+  dx = dy = [0.4, 0.4, 0.4]
   dz = [positive, negative, neutral]
   
   colors = ['#4CAF50', '#F44336', '#FFC107']
@@ -213,19 +211,16 @@ elif selection == "Search Restaurants":
   
   ax.set_xticks([0.25, 1.25, 2.25])
   ax.set_xticklabels(['Positive', 'Negative', 'Neutral'])
-  ax.set_xlabel('Sentiment')
   ax.set_ylabel('')
   ax.set_zlabel('Score')
   ax.set_yticks([])
   
-  ax.set_title('Sentiment Analysis - 3D Bar Chart')
+  ax.set_title(f"{selected_restaurant} \nSentiment Analysis - 3D Bar Chart")
   st.pyplot(fig)
   
-  
+  st.write("\n # ")
   
   # Pie Chart
-  st.write("#### Sentiment Analysis - Pie Chart")
-  
   labels = ['Positive', 'Negative', 'Neutral']
   sizes = [positive, negative, neutral]
   colors = ['#4CAF50', '#F44336', '#FFC107']
@@ -235,8 +230,85 @@ elif selection == "Search Restaurants":
   ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
   
   ax1.axis('equal')
+  ax1.set_title(f"{selected_restaurant} \nSentiment Analysis - Pie Chart")
   st.pyplot(fig1)
   
+  st.write("\n # ")
+  
+  sentiment_df.set_index('Sentiment', inplace=True)
+  fig2, ax2 = plt.subplots()
+  sentiment_df.T.plot(kind='bar', stacked=True, color=colors, ax=ax2)
+  ax2.set_title(f"{selected_restaurant} \nSentiment Analysis - Stacked Bar Chart")
+  ax2.set_ylabel('Score')
+  ax2.legend(title='Sentiment')
+  st.pyplot(fig2)
+  
+  
+  
+  st.write("\n # ")
+  
+  
+  selected_restaurant_df = df[df['restaurant'] == selected_restaurant]
+  
+  positive_threshold = 4
+  negative_threshold = 2
+  
+  
+  positive_text = ' '.join(selected_restaurant_df[selected_restaurant_df['ratings_int'] >= positive_threshold]['preprocessed_text'])
+  negative_text = ' '.join(selected_restaurant_df[selected_restaurant_df['ratings_int'] <= negative_threshold]['preprocessed_text'])
+  neutral_text = ' '.join(selected_restaurant_df[(selected_restaurant_df['ratings_int'] > negative_threshold) & (selected_restaurant_df['ratings_int'] < positive_threshold)]['preprocessed_text'])
+  
+  
+  positive_wc = WordCloud(width=1600, height=800, background_color='white', colormap='Greens').generate(positive_text)
+  negative_wc = WordCloud(width=1600, height=800, background_color='white', colormap='Reds').generate(negative_text)
+  neutral_wc = WordCloud(width=1600, height=800, background_color='white', colormap='Oranges').generate(neutral_text)
+  
+  fig3, axs = plt.subplots(1, 3, figsize=(14, 6))
+  
+  axs[0].imshow(positive_wc, interpolation='bilinear')
+  axs[0].axis('off')
+  axs[0].set_title('Positive Sentiment Word Cloud')
+  
+  axs[1].imshow(negative_wc, interpolation='bilinear')
+  axs[1].axis('off')
+  axs[1].set_title('Negative Sentiment Word Cloud')
+  
+  axs[2].imshow(neutral_wc, interpolation='bilinear')
+  axs[2].axis('off')
+  axs[2].set_title('Neutral Sentiment Word Cloud')
+  
+  fig3.suptitle(f"{selected_restaurant} \nSentiment Analysis - Word Clouds", fontsize=26)
+  
+  st.pyplot(fig3)
+  
+  
+  st.write("\n # ")
+  
+  
+  overall_sentiment = 'Positive' if positive > max(neutral, negative) else 'Neutral' if neutral > max(positive, negative) else 'Negative'
+  
+  if overall_sentiment == "Positive":
+    sentiment_color = "green"
+  elif overall_sentiment == "Neutral":
+    sentiment_color = "orange"
+  else:
+    sentiment_color = "red"
+  
+  # """
+  html_3d_text = f"""
+  <div style="text-align: center; background-color: {sentiment_color}; border-radius: 10px; padding: 20px;">
+    <h2 style="
+        font-family: 'Arial', sans-serif;
+        font-size: 50px;
+        color: white;  /* Text color */
+        text-shadow: 4px 4px 0 rgba(26, 26, 26, 0.5), -2px -2px 0 rgba(26, 26, 26, 0.5), 2px -2px 0 rgba(26, 26, 26, 0.5), -2px 2px 0 rgba(26, 26, 26, 0.5);
+    ">
+        Overall Sentiment: <strong>{overall_sentiment}</strong>
+    </h2>
+</div>
+    """
+  
+  st.markdown(html_3d_text, unsafe_allow_html=True)
     
 elif selection == "Dataset Overview":
     st.subheader("Dataset Overview")
